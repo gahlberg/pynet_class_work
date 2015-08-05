3b075280a2396eee66aed5064380fdfcc1814470
Geoff Ahlberg <gahlberg@juniper.net>
2015-08-05 05:47:26 -0700

    Adding file for Excercise 4.

---
+++ ex4_add_file.py
@@ -0,0 +1,9 @@
+#!/usr/local/bin/python
+"""Counts the vowels in a user input string."""
+
+s = input("Enter any string: ")
+vcount = 0
+for c in s:
+    if c in "aeiouAIEOU":
+        vcount += 1
+print("Vowel count:", vcount)
