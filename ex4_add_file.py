240ebbfe2a794fe2e433556a1b5c01a40f212950
gahlberg <gahlberg@juniper.net>
2015-08-05 09:01:40 -0400

    Update ex4_add_file.py

--- ex4_add_file.py
+++ ex4_add_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 """Counts the vowels in a user input string."""
 
-s = input("Enter any string: ")
+s = input('Enter any string: ')
 vcount = 0
 for c in s:
-    if c in "aeiouAIEOU":
+    if c in 'aeiouAIEOU':
         vcount += 1
-print("Vowel count:", vcount)
+print('Vowel count:', vcount)
